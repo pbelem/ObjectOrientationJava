42630d624b3ca18cff362c32c374d6e4a14c8a03
Pedro Belém <pedro.belem@ucsal.edu.br>
2024-11-04 19:37:59 -0300

    Add files via upload

---
+++ learningJavaPOO/src10/entities/Accounts.java
@@ -0,0 +1,40 @@
+package entities;
+
+public class Accounts {
+
+	private int accountNum;
+	private String nameAccount;
+	private Double currentValue;
+
+	public Accounts(Double currentValue) {
+		this.currentValue = currentValue;
+	}
+
+	public Accounts(int accountNum, String nameAccount, Double initialDeposit) {
+		this.accountNum = accountNum;
+		this.nameAccount = nameAccount;
+		this.currentValue = initialDeposit;
+	}
+
+	public Accounts() {
+	}
+
+	public void setNameAccount(String nameAccount) {
+		this.nameAccount = nameAccount;
+	}
+
+	public Double getCurrentValue() {
+		return currentValue;
+	}
+
+	public void setCurrentValue(Double currentValue) {
+		this.currentValue = currentValue;
+	}
+
+	public String toString() {
+		return String.format("Account data:%nAccount %s, Holder %s, Balance %.2f", accountNum, nameAccount,
+				currentValue);
+
+	}
+
+}
