e868def6221c32b75a656a6083afa94e68b6afad
pbelem <pedro.belem@ucsal.edu.br>
2024-11-11 16:05:59 -0300

    Salvando versao no repositorio git

---
+++ src12/aplication/Program3.java
@@ -0,0 +1,18 @@
+package aplication;
+
+import java.util.Locale;
+
+import util.Operation2;
+
+public class Program3 {
+
+	public static void main(String[] args) {
+		Locale.setDefault(Locale.US);
+
+		Operation2.Register();
+
+		Operation2.manageAccount();
+
+	}
+
+}
