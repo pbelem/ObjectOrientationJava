50b71176c73d62b419eabee89e9a32f4b1194a26
pbelem <pedro.belem@ucsal.edu.br>
2025-02-24 18:08:53 -0300

    solved exercises

---
+++ src21/Exercises/calculator/MathOperations.java
@@ -0,0 +1,24 @@
+package calculator;
+
+public class MathOperations {
+
+    public static double multiply(double a, double b) {
+        return a * b;
+    }
+    public static double divide(double a, double b) {
+        return a / b;
+    }
+    public static double add(double a, double b) {
+        return a + b;
+    }
+    public static double subtract(double a, double b) {
+        return a - b;
+    }
+    public static double averageOfTwo(double a, double b) {
+        return (a + b) / 2;
+    }
+    public static double averageOfThree(double a, double b, double c) {
+        return (a + b + c) / 3;
+    }
+
+}
