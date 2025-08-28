d9eaaca60813534bd2c7e2f6e36b26fd1fe1a350
pbelem <pedro.belem@ucsal.edu.br>
2025-08-28 19:44:16 -0300

    updating Polymorphism

--- Polymorphism/src/entities/employee/OutsourceEmployee.java
+++ Polymorphism/src/entities/employee/OutsourceEmployee.java
@@ -2,14 +2,15 @@ package entities.employee;
 
 public class OutsourceEmployee extends Employee{
 
-    private Double additionalCharge = 1.1;
+    private Double additionalCharge = 0.0;
 
-    public OutsourceEmployee(String nameEmployee, Integer hours, Double valuePerHour) {
+    public OutsourceEmployee(String nameEmployee, Integer hours, Double valuePerHour, Double additionalCharge) {
         super(nameEmployee, hours, valuePerHour);
+        this.additionalCharge = additionalCharge;
     }
 
     @Override
     public Double payment() {
-        return (super.getValuePerHour() * super.getHours()) * additionalCharge;
+        return (super.getValuePerHour() * super.getHours()) + (additionalCharge * 1.1);
     }
 }
--- Polymorphism/src/util/Read.java
+++ Polymorphism/src/util/Read.java
@@ -14,25 +14,39 @@ public class Read {
         Scanner input = new Scanner(System.in);
 
         for (int I = 0, x = 1; I < 1; I++) {
-            System.out.println("How many employees?");
+            System.out.print("Enter the number of employees: ");
             x = Integer.parseInt(input.nextLine());
 
             for (int II = 0; II < x; II++) {
-                System.out.println("Is it an employee(1) or outsourced(2)?");
+                System.out.println(" Employee #" + II + " data:");
+                System.out.print("Is it an employee(1) or outsourced(2)?");
                 int option = Integer.parseInt(input.nextLine());
-                System.out.print("Enter the name of the employee : ");
-                String name = input.nextLine();
-                System.out.println("How many hours?");
-                int hours = Integer.parseInt(input.nextLine());
-                System.out.println("What is the value per hour?");
-                Double perHour = Double.parseDouble(input.nextLine());
 
                 if (option == 1) {
+                    System.out.print("Name: ");
+                    String name = input.nextLine();
+                    System.out.print("Hours: ");
+                    int hours = Integer.parseInt(input.nextLine());
+                    System.out.print("Value per hour: ");
+                    Double perHour = Double.parseDouble(input.nextLine());
+
                     Employee employee = new Employee(name, hours, perHour);
                     employeeList.addEmployee(employee);
                 } else if (option == 2) {
-                    Employee employee = new OutsourceEmployee(name, hours, perHour);
+                    System.out.print("Name: ");
+                    String name = input.nextLine();
+                    System.out.print("Hours: ");
+                    int hours = Integer.parseInt(input.nextLine());
+                    System.out.print("Value per hour: ");
+                    Double perHour = Double.parseDouble(input.nextLine());
+                    System.out.print("Additional Charge: ");
+                    Double additionalCharge = Double.parseDouble(input.nextLine());
+
+                    Employee employee = new OutsourceEmployee(name, hours, perHour, additionalCharge);
                     employeeList.addEmployee(employee);
+                } else {
+                    System.out.print("Invalid option");
+                    II--;
                 }
 
             }
